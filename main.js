b31a8167ca7c9f946d9bc4e62733f0ec49ca75ce
Greg Johnson <greg.johnson@gibson.com>
2023-02-28 10:19:50 -0600

    Initial Import

---
+++ main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+import * as THREE from "three";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { gsap } from "gsap";
+import { Object3D, SpotLightHelper } from "three";
